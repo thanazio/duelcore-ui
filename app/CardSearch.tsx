3cc81971ad2bc574cc8ec0245befcec32cbed524
Thanazio <thanazio@proton.me>
2025-01-23 21:50:36 +0200

    feat: display hovered card data

--- app/CardSearch.tsx
+++ app/CardSearch.tsx
@@ -6,11 +6,63 @@ import { useState, useEffect, ChangeEvent } from "react";
 import useBoolean from "@/hooks/useBoolean";
 import useDebounce from "@/hooks/useDebounce";
 import { CardType } from "@/types/CardType";
+import CardData from "@/components/CardData";
+
+
+const acrobaticMagician = {
+    "id": 33656832,
+    "name": "Acrobatic Magician",
+    "typeline": [
+        "Spellcaster",
+        "Pendulum",
+        "Effect"
+    ],
+    "type": "Pendulum Effect Monster",
+    "humanReadableCardType": "Pendulum Effect Monster",
+    "frameType": "effect_pendulum",
+    "desc": "[ Pendulum Effect ] \nWhen a monster(s) you control is destroyed by a card effect (except during the Damage Step): You can Special Summon this card from your Pendulum Zone. You can only use this effect of \"Acrobatic Magician\" once per turn.\n\n[ Monster Effect ] \nIf the activation of a Spell/Trap Card is negated (except during the Damage Step): You can Special Summon this card from your hand. When this card is destroyed by battle: You can place this card in your Pendulum Zone.",
+    "race": "Spellcaster",
+    "pend_desc": "When a monster(s) you control is destroyed by a card effect (except during the Damage Step): You can Special Summon this card from your Pendulum Zone. You can only use this effect of \"Acrobatic Magician\" once per turn.",
+    "monster_desc": "If the activation of a Spell/Trap Card is negated (except during the Damage Step): You can Special Summon this card from your hand. When this card is destroyed by battle: You can place this card in your Pendulum Zone.",
+    "atk": 800,
+    "def": 2300,
+    "level": 5,
+    "attribute": "DARK",
+    "archetype": "Magician",
+    "scale": 2,
+    "ygoprodeck_url": "https://ygoprodeck.com/card/acrobatic-magician-2854",
+    "card_sets": [
+        {
+            "set_name": "The Dark Illusion",
+            "set_code": "TDIL-EN009",
+            "set_rarity": "Rare",
+            "set_rarity_code": "(R)",
+            "set_price": "0"
+        }
+    ],
+    "card_images": [
+        {
+            "id": 33656832,
+            "image_url": "https://images.ygoprodeck.com/images/cards/33656832.jpg",
+            "image_url_small": "https://images.ygoprodeck.com/images/cards_small/33656832.jpg",
+            "image_url_cropped": "https://images.ygoprodeck.com/images/cards_cropped/33656832.jpg"
+        }
+    ],
+    "card_prices": [
+        {
+            "cardmarket_price": "0.10",
+            "tcgplayer_price": "0.25",
+            "ebay_price": "1.25",
+            "amazon_price": "0.25",
+            "coolstuffinc_price": "0.25"
+        }
+    ]
+};
 
 
 export default function CardSearch() {
     const [searchValue, setSearchValue] = useState('');
-    const [hoveredCard, setHoveredCard] = useState<CardType | null>(null);
+    const [hoveredCard, setHoveredCard] = useState<CardType | null>(acrobaticMagician);
     const debouncedSearchValue = useDebounce(searchValue);
     const [searchResults, setSearchResults] = useState<CardType[]>([]);
     const {
@@ -57,12 +109,12 @@ export default function CardSearch() {
     }
 
     return (
-        <div className="grid grid-cols-3 gap-2 h-screen">
-            <div className="flex items-center flex-col bg-red-400">
+        <div className="flex justify-center gap-2 max-h-screen mx-auto">
+            <div className="flex items-center flex-col w-1/3 overflow-x-auto overflow-y-scroll bg-red-400">
                 <input type="text" className="text-black" value={searchValue} onChange={handleSearchValue} placeholder="Search for a card" />
-                {isLoading && <h1>Now Loading</h1>}
-                <div className="grid grid-cols-5 gap-1 overflow-x-hidden overflow-y-auto">
-                    {!isLoading && searchResults.map((card) =>
+                {isLoading && searchResults.length === 0 && <h1>Now Loading</h1>}
+                <div className="grid grid-cols-5 gap-1">
+                    {searchResults.length > 0 && searchResults.map((card) =>
                         <div
                             key={card.id}
                             className="border-slate-400 border-2"
@@ -74,8 +126,8 @@ export default function CardSearch() {
                     )}
                 </div>
             </div>
-            <div className="grid grid-cols-5 w-full gap-2 overflow-x-hidden overflow-y-auto bg-blue-400">
-                <h1>MY DECK</h1>
+            <div className=" w-1/3 bg-blue-400">
+                {hoveredCard && hoveredCardFlag && <CardData card={hoveredCard} />}
             </div>
         </div>
     );
---
+++ components/CardData.tsx
@@ -0,0 +1,39 @@
+import { CardType } from "@/types/CardType";
+import Image from "next/image";
+
+
+export default function CardData({ card }: { card: CardType}) {
+    if (!card) {
+        return <p>Select a card</p>
+    }
+
+    const {
+        cardmarket_price,
+        tcgplayer_price,
+        ebay_price,
+        amazon_price,
+        coolstuffinc_price
+    } = card.card_prices[0];
+
+    console.log(card);
+
+    return (
+        <div className="p-2 flex flex-col justify-center items-center">
+            <Image src={card.card_images[0].image_url_small} width={268} height={391} alt={card.name} />           
+            <div>
+                <p>{card?.attribute} {card.level} {card.scale}</p>
+                <p>{card.type}</p>
+                <p>{card.desc.split('\n').map((line, index) => (<span key={index}>{line}<br /></span>))}</p>
+                <p>ATK: {card.atk} DEF: {card.def}</p>
+            </div>
+            <div>
+                <h3>Prices (may be out of date)</h3>
+                <p>Card Market Price: {cardmarket_price}</p>
+                <p>TCG Player Price: {tcgplayer_price}</p>
+                <p>Ebay Price: {ebay_price}</p>
+                <p>Amazon Price: {amazon_price}</p>
+                <p>Cool Stuff inc. Price: {coolstuffinc_price}</p>
+            </div>
+        </div>
+    )
+}
